188b6ee045a508912dc059e76cb1ecf55859521f
maplesyrupman <maplesyrupman@protonmail.com>
2022-04-07 16:18:55 -0400

    project setup, added under construction page will remove later

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  content: [
+    "./pages/*.js",
+    "./pages/**/*.js",
+     "./components/*.js"
+   ],
+  theme: {
+    container: {
+      center: true
+    },
+  },
+  plugins: [],
+}

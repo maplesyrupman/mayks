3fd7ef3c8d87c8cee36a42800b52eb16da7a23b8
maplesyrupman <maplesyrupman@protonmail.com>
2022-04-21 21:01:41 -0400

    under construction page

---
+++ pages/index.js
@@ -0,0 +1,25 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='under-construction'>
+      <Head>
+        <title>Tedesco Group</title>
+      </Head>
+      <div className='flex flex-col justify-center items-center pt-6'>
+        <div className=''>
+          <Image src='/images/logo.png' width={500} height={250} />
+        </div>
+        <div className='p-4 '>
+        <h1 className='text-3xl text-center'>
+          Coming Soon!
+        </h1>
+        <p className='text-center'>
+          Our website is currently under construction, please check back shortly. 
+        </p>
+        </div>
+      </div>
+    </div>
+  )
+}
